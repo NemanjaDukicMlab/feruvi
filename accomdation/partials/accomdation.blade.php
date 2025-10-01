77e90376a5b24e8950a9b27ae0913f3ad797b092
NemanjaDukicMlab <nemanjad@mlab-studio.com>
2025-10-01 18:16:24 +0200

    Create accomdation.blade.php

---
+++ accomdation/partials/accomdation.blade.php
@@ -0,0 +1,70 @@
+<section class="small-banner | js-top-banner">
+    <div class="small-banner_inner container">
+        <h1 class="small-banner_title sora-72-600">Moj smeštaj</h1>
+        <div class="small-banner_text sora-16-400" style="max-width: 900px; margin-top: 20px;"></div>
+    </div>
+</section>
+<section class="main">
+
+    <aside>
+</aside>
+<main>
+    <div class="view">
+    </div>
+    <div class="status">
+    </div>
+    <div class="reservation">
+    </div>
+</main>
+
+
+</section>
+
+
+
+
+
+<style>
+section.main {
+    display:flex;
+    padding:20px;
+}
+
+aside {
+width:20%;
+border-right:1px solid;
+background:rgba(200, 209, 225, 0.8);
+padding:10px;
+}
+
+main {
+width:70%;
+margin-left:5%
+}
+
+main .view {
+padding: 20px;
+    border: 1px solid;
+    border-radius: 10px;
+    height: 20vh;
+}
+
+main .status {
+background:rgba(200, 209, 225, 0.8);
+padding: 20px;
+    border: 1px solid;
+    border-radius: 10px;
+    height:45vh;
+    margin-top:20px;
+}
+
+main .reservation {
+padding: 20px;
+    border: 1px solid;
+    border-radius: 10px;
+    height: 45vh;
+        margin-top:20px;
+
+}
+
+</style>
